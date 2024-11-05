07407881ea4f86a8a00303f5daf2d5fe6a396d16
Steven Palfreyman <ironmonkus@gmail.com>
2024-11-05 14:42:57 -0500

    Code finished

--- AirportLockerRental/Program.cs
+++ AirportLockerRental/Program.cs
@@ -1,8 +1,8 @@
 ﻿/*----------------------------------------------------------------------------------------------------------
- | Name: Airport Locker Rental Menu  
+ | Project: Airport Locker Rental 
  | Description: A menu of five options to view, rent, end a locker rental, list all locker contents, or quit.
- | Author: Steven Palfreyman
- | Dependencies: N/A
+ | Author: SkillFoundry -- coded by Steve
+ | Dependencies: Null
  | Version: 1.0
  -----------------------------------------------------------------------------------------------------------*/
 
@@ -10,8 +10,9 @@
 /*----------------------
  |  Declare variables
  -----------------------*/
-int menuChoice = 0, lockerChoice;
-string viewLockerReturn, lockerContents, lockerContentsReturn;
+int numberChoice = 0;
+string viewLockerReturn, lockerContents, lockerContentsReturn, endLockerReturn, listLockerReturn = "";
+
 
 /*--------------------------
  |  Array for locker storage
@@ -22,7 +23,7 @@ string[] lockerStorage = new string[100];
 /*----------------------------
  |  Start -- Display main menu
  -----------------------------*/
-while (menuChoice != 5)
+while (numberChoice != 5)
 {
     // Clear console before displaying the menu
     Console.Clear();
@@ -37,29 +38,11 @@ while (menuChoice != 5)
     Console.WriteLine("5. Quit");
     Console.WriteLine();
 
-    // Input validation
-    do
-    {
-        Console.Write("Enter your choice (1-5): ");
-        if (int.TryParse(Console.ReadLine(), out menuChoice))
-        {
-            if (menuChoice > 0 && menuChoice <=5)
-            {
-                break;
-            }
-            else
-            {
-                Console.WriteLine("Invalid choice.  Please enter a number from 1 to 5.");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Not a number!");
-        }
-    }
-    while (true);
+    // Prompt for list number
+    promptForNumber(5);
 
-    switch (menuChoice)
+  
+    switch (numberChoice)
     {
         case 1:
             Console.WriteLine(viewALocker());
@@ -72,39 +55,53 @@ while (menuChoice != 5)
         case 3:
             Console.WriteLine(endALocker());
             break;
+
+        case 4:
+            Console.WriteLine();
+            Console.WriteLine(listAllLocker());
+            break;
     }
 
     Console.WriteLine("\nPress any key to continue...");
     Console.ReadKey();
 }
 
-/*--------------------------
- |  Prompt for locker number
- ---------------------------*/
-int promptForLocker()
+
+/*------------------------------------
+ |  Return number for menu and lockers
+ -------------------------------------*/
+int promptForNumber(int num)
 {
+
     do
     {
-        Console.Write("Enter locker number (1-100): ");
-        if(int.TryParse(Console.ReadLine(),out lockerChoice))
+        if (num == 5)
         {
-            if (lockerChoice > 0 && lockerChoice <= 100)
+            Console.Write("Enter your choice (1-5): ");
+        }
+        if (num == 100)
+        {
+            Console.Write("Enter locker number (1-100): ");
+        }
+        if (int.TryParse(Console.ReadLine(), out numberChoice))
+        {
+            if (numberChoice > 0 && numberChoice <= num)
             {
-                break; 
+                break;
             }
+
             else
             {
-                Console.WriteLine("Invalid choice.  Please enter a number from 1 to 100");
+                Console.WriteLine("Invalid choice.");
             }
         }
         else
         {
             Console.WriteLine("Not a number!");
         }
-    }
-    while (true);
+    } while (true);
 
-    return lockerChoice;
+    return numberChoice;
 }
 
 
@@ -114,44 +111,44 @@ int promptForLocker()
 string viewALocker()
 {
     // Prompt for locker number
-    promptForLocker();
+    promptForNumber(100);
 
     // Display the contents of the locker
-    if (string.IsNullOrEmpty(lockerStorage[lockerChoice]))
+    if (string.IsNullOrEmpty(lockerStorage[numberChoice]))
     {
-        viewLockerReturn = $"Locker {lockerChoice} is EMPTY.";
+        viewLockerReturn = $"Locker {numberChoice} is EMPTY.";
     }
     else
     {
-        viewLockerReturn = $"Locker {lockerChoice} contents: {lockerStorage[lockerChoice]}";
+        viewLockerReturn = $"Locker {numberChoice} contents: {lockerStorage[numberChoice]}";
     }
     
     return viewLockerReturn;
 }
+
 
 /*----------------------
  |  Rent a Locker
  -----------------------*/
 string rentALocker()
 {
-    
     // Prompt for locker number
-    promptForLocker();
+    promptForNumber(100);
 
-    if (!string.IsNullOrEmpty(lockerStorage[lockerChoice]))
+    if (!string.IsNullOrEmpty(lockerStorage[numberChoice]))
     {
-        lockerContentsReturn = $"Sorry, but locker {lockerChoice} has already been rented!";
+        lockerContentsReturn = $"Sorry, but locker {numberChoice} has already been rented!";
     }
     else
     {
         // Prompt user for what to store in the locker
-        Console.WriteLine("Enter the item you want to store in the locker: ");
+        Console.Write("Enter the item you want to store in the locker: ");
         lockerContents = Console.ReadLine();
 
-        // Add lockerChoice to lockerStorage
-        lockerStorage[lockerChoice] = lockerContents;
+        // Add numberChoice to lockerStorage
+        lockerStorage[numberChoice] = lockerContents;
 
-        lockerContentsReturn = $"Locker {lockerChoice} has been rented for {lockerContents} storage.";
+        lockerContentsReturn = $"Locker {numberChoice} has been rented for {lockerContents} storage.";
     }
    
     return lockerContentsReturn;
@@ -163,15 +160,40 @@ string rentALocker()
  -----------------------*/
 string endALocker()
 {
+    // Prompt for locker number
+    promptForNumber(100);
 
+    if (string.IsNullOrEmpty(lockerStorage[numberChoice]))
+    {
+        endLockerReturn = $"Locker {numberChoice} is not currently rented.";
+    }
+    else
+    {
+        lockerContents = lockerStorage[numberChoice];
+        lockerStorage[numberChoice] = null;
+        endLockerReturn = $"Locker {numberChoice} rental has ended, please take your {lockerContents}.";
+    }
+
+    return endLockerReturn;
 }
-
 
 
 /*--------------------------
  |  List all Locker Contents
  ---------------------------*/
+string listAllLocker()
+{
+    listLockerReturn = "";
+    for (int i = 0; i < lockerStorage.Length; i++)
+    {
+        if (!string.IsNullOrEmpty(lockerStorage[i]))
+        {
+            listLockerReturn += $"Locker {i}: {lockerStorage[i]}\n";
+        }
+    }
 
+    return listLockerReturn;    
+}
 
 
 
